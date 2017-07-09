558ceb02eb143251cf5f12401663093e61c12ab2
Le Tuan Anh <tuananh.ke@gmail.com>
2017-07-09 16:23:28 +0800

    Redirect to shinchanjs root

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect, url_for
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_folder='shinchanjs')
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('static', filename='index.html'))
 
 
 if __name__ == "__main__":
